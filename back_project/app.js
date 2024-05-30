f8302923c47eaa15e9ff16b26467c6951e872315
윤재이 <yunjaei@MACBOOK-YUNJAY.local>
2024-05-30 18:38:44 +0900

    login, main, signup page

--- back_project/app.js
+++ back_project/app.js
@@ -1,17 +1,20 @@
-// Import the Express module
 const express = require('express');
+const path = require('path');
 
-// Create an Express application
 const app = express();
 
-// Define a route
+// 정적 파일을 제공하는 미들웨어 설정
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.sendFile('/Users/yunjaei/SVPG_PJ/front_project/index.html');
+  res.sendFile(path.join(__dirname, 'public/login.html'));
 });
-app.get('/enter', function(req, res){
-  res.sendFile('/Users/yunjaei/SVPG_PJ/front_project/enter.html');
-})
-// Start the server
+app.get('/signUp', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/signUp.html'));
+});
+app.get('/main', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/main.html'));
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
